c3caba647991051f9ee66fb633bb3fb3a26287f0
iushaheed <iushaheed@gmail.com>
2016-10-17 13:00:59 +0600

    IUS|IP ADDRESS ADDED (WiFi)

--- app/src/main/java/net/teambinary/myconfig/MainActivity.java
+++ app/src/main/java/net/teambinary/myconfig/MainActivity.java
@@ -1,13 +1,29 @@
 package net.teambinary.myconfig;
 
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Formatter;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView ipData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ipData= (TextView) findViewById(R.id.IPdata);
+
+        WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+        int ip = wifiInfo.getIpAddress();
+        String ipAddress = Formatter.formatIpAddress(ip);
+
+        ipData.setText(ipAddress);
+
+
     }
 }
